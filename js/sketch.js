292eefef32ff2d3b58b99c66917699513218a5ee
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2021-10-29 10:53:52 -0400

    site layout complete

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,9 @@
 function setup(){
-
+    createCanvas(windowWidth-(windowWidth*.05),windowHeight-(windowHeight*.25));
+    stroke(255);
+    frameRate(30);
 }
 
 function draw() {
-    
+    background(0);
 }
