ea68a3002d0841ffaae47aebfd166b25f1934b80
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2021-11-01 16:25:41 -0400

    cleaned up css + howto

--- js/sketch.js
+++ js/sketch.js
@@ -1,29 +1,34 @@
 function setup(){
     createCanvas(windowWidth-(windowWidth*.03),windowHeight-(windowHeight*.13));
     stroke(255);
-    frameRate(30);
+    // frameRate(30);
 }
+
+let x = 0;
+let y = 0;
 
 function draw() {
     background(0);
+    x = lerp(x,mouseX, 0.05)
+    y = lerp(y,mouseY, 0.05)
     // textAlign(CENTER);
     textFont('Chelsea Market')
     textSize(20);
-    textAlign(RIGHT)
-    const beginningText = "Hello, World!"
+    textAlign(CENTER)
+    const beginningText = "Press any key to begin"
     fill(255)
     text(beginningText,windowWidth * .5,windowHeight * .25);
     
+    ellipse(x, y, 66, 66);
     
     // textAlign(RIGHT);
-    text('ABCD', 50, 30);
-    textAlign(CENTER);
-    text('EFGH', 50, 50);
+    // text('ABCD', 50, 30);
+    // textAlign(CENTER);
+    // text('EFGH', 50, 50);
     // textAlign(LEFT);
-    text('IJKL', 50, 70);
+    // text('IJKL', 50, 70);
     // clear background
     // background(0)
+    
 }
-function drawText(){
-    return "hello"
-}
+
