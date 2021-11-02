fa939d876f5733deceb95aa8910808bde1aba448
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2021-11-02 13:06:11 -0400

    field flow + vect

--- js/flowfield.js
+++ js/flowfield.js
@@ -1,14 +1,14 @@
 // flowfield.js
 
 class FlowField{
-    constructor(vectorSize){
+    constructor(density){
         // calculate density of FlowField (smaller # = more dense)
-        this.vectorSize = vectorSize
-        // calculate row + column count based on vectorSize 
-        this.rows = height / this.vectorSize
-        this.columns = width / this.vectorSize
+        this.density = density;
+        // calculate row + column count based on density 
+        this.rows = height / this.density;
+        this.columns = width / this.density;
         // create 2d array of rows x columns
-        this.field = this.generate2DArray(this.columns)
+        this.field = this.generate2DArray(this.columns);
         this.initializeField();
     }
 
@@ -18,26 +18,47 @@ class FlowField{
     }
 
     initializeField(){
-        let xOff = 0
+        let xOff = 0;
         // Give me legions of fields. 100000 to be precise.
-        NoiseSeed(Math.floor(random(10000)))
+        NoiseSeed(Math.floor(random(10000)));
 
         for(let i = 0; i < this.columns; i++) {
-            let yOff = 0
+            let yOff = 0;
             for(let j = 0; j< this.rows; j++) {
                 // Polar --> Cartesian
-                let theta = map(noise(xOff,yOff), 0, 1, 0, TWO_PI)
-                this.field[i][j] = createVector(cos(theta), sin(theta))
+                let theta = map(noise(xOff,yOff), 0, 1, 0, TWO_PI);
+                this.field[i][j] = createVector(cos(theta), sin(theta));
             }
-            yOff += 0.1
+            yOff += 0.1;
         }
-        xOff += 0.1
+        xOff += 0.1;
     }
 
-    drawVect(x, y, vect, fieldDensity){
-        // Start new drawing state for vector
-        push();
+    lookup(position) {
+        let column = Math.floor(constrain(position.x / this.density, 0, this.columns - 1));
+        let row = Math.floor(constrain(position.y / this.density, 0, this.rows - 1));
+        return this.field[column][row].copy();
+    }
+    
+    displayField(){
+        for(let i = 0; i < this.columns; i++) {
+            for(let j = 0; j < this.rows; j++) {
+                this.drawFieldVector(this.field[i][j], i * this.density, j * this.density, this.density - 2);
+            }
+        }
+    }
 
+    drawFieldVector(x, y, vect, fieldDensity){
+        // Start new drawing state for fieldVector
+        push();
+        let arrowSize = 4;
+        translate(x,y);
+        stroke(200,100);
+        rotate(vect.heading());
+        let length = vect.mag() * fieldDensity;
+        line(0, 0, length, 0);
+        line(len,0,length - arrowsize, + arrowsize / 2);
+        line(len,0,length - arrowsize, - arrowsize / 2);
         pop();
     }
 
--- js/vect.js
+++ js/vect.js
@@ -3,28 +3,80 @@
 class Vect {
     constructor(x, y, maxSpeed, maxForce) {
         // x, y position
-        this.x = x
-        this.y = y
+        this.x = x;
+        this.y = y;
         // speed, force will be randomly generated when initialized
-        self.maxSpeed = maxSpeed
-        self.maxForce = maxForce
+        self.maxSpeed = maxSpeed;
+        self.maxForce = maxForce;
         // position vector
-        this.position = createVector(x,y)
+        this.position = createVector(x,y);
         // acceleration, velocity will be determined by vect + FlowField position vectors
-        this.acceleration = createVector(0,0)
-        this.velocity = createVector(0,0)
+        this.acceleration = createVector(0,0);
+        this.velocity = createVector(0,0);
         // size of vect (subject to change)
-        this.r = 3
+        this.r = 3;
     }
 
-    display(){
-        // SHAPE TINKERING TBD
-        // beginShape();
-        // vertex(0, -this.r );
-        // vertex(-this.r, this.r*2);
-        // vertex(0,this.r*1.5)
-        // vertex(this.r, this.r*2);
-        // endShape(CLOSE);
+    initializeVect(){
+        this.updateVectLocation();
+        this.fieldWrap();
+        this.drawVect();
     }
 
+    updateVectForce(force){
+        this.acceleration.add(force);
+    }
+
+    updateVectLocation(){
+        // update + limit velocity
+        this.velocity.add(this.acceleration);
+        this.velocity.limit(this.maxSpeed);
+
+        // update position
+        this.position.add(this.velocity);
+
+        // reset acceleration
+        this.acceleration.mult(0);
+    }
+
+    // Magic method
+    follow(flow) {
+        let desired = flow.lookup(this.position);
+        desired.multi(this.maxSpeed);
+        let steer = p5.Vector.sub(desired, this.velocity);
+        steer.limit(this.maxForce);
+        this.updateVectForce.steer();
+    }
+
+    // telport vector to opposite edge if travels off canvas
+    fieldWrap(){
+        if(this.position.x < - this.r){
+            this.position.x = width + this.r;
+        } else if(this.position.y < -this.r) {
+            this.position.y = height + this.r;
+        } else if(this.position.x > this.r) {
+            this.position.x = -this.r;
+        } else if(this.position.y > this.r) {
+            this.position.y = -this.r;
+        }
+    }
+
+    drawVect(){
+        // Start new drawing state for vector
+        push();
+        let theta = this.velocity.heading() + PI / 2;
+        fill(127);
+        stroke(200);
+        strokeWeight(1);
+        push();
+        translate(this.position.x, this.position.y);
+        rotate(theta);
+        beginShape();
+        vertex(0, -this.r);
+        vertex(-this.r, this.r * 2);
+        vertex(0, this.r * 1.5);
+        vertex(this.r, this.r * 2);
+        endShape(CLOSE);
+        pop();
+    }
 }
