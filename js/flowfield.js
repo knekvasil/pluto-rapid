aad319eb84a93b004fdf04c2b10fdd1c8beb7b01
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2021-11-02 11:52:03 -0400

    init flowfield + no errors

--- js/flowfield.js
+++ js/flowfield.js
@@ -9,10 +9,36 @@ class FlowField{
         this.columns = width / this.vectorSize
         // create 2d array of rows x columns
         this.field = this.generate2DArray(this.columns)
+        this.initializeField();
     }
 
-    // thanks Javascript
+    // Thanks JavaScript! Very cool.
     generate2DArray(n){
-        return Array(n).fill(Array(0).fill())
+        return Array(n).fill(Array(0).fill());
     }
+
+    initializeField(){
+        let xOff = 0
+        // Give me legions of fields. 100000 to be precise.
+        NoiseSeed(Math.floor(random(10000)))
+
+        for(let i = 0; i < this.columns; i++) {
+            let yOff = 0
+            for(let j = 0; j< this.rows; j++) {
+                // Polar --> Cartesian
+                let theta = map(noise(xOff,yOff), 0, 1, 0, TWO_PI)
+                this.field[i][j] = createVector(cos(theta), sin(theta))
+            }
+            yOff += 0.1
+        }
+        xOff += 0.1
+    }
+
+    drawVect(x, y, vect, fieldDensity){
+        // Start new drawing state for vector
+        push();
+
+        pop();
+    }
+
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,27 +1,47 @@
 // sketch.js
 
-function setup(){
-    createCanvas(windowWidth-(windowWidth*.03),windowHeight-(windowHeight*.13));
-    stroke(255);
-    // frameRate(30);
-}
-
 let x = 0;
 let y = 0;
 
+
+function setup(){
+    createCanvas(windowWidth-(windowWidth*.03),windowHeight-(windowHeight*.13));
+    stroke(255);
+}
+
 function draw() {
-    background(0);
-    x = lerp(x,mouseX, 0.05)
-    y = lerp(y,mouseY, 0.05)
-    // textAlign(CENTER);
-    textFont('Chelsea Market')
-    textSize(20);
-    textAlign(CENTER)
-    const beginningText = "Press any key to begin"
-    fill(255)
-    text(beginningText,windowWidth * .5,windowHeight * .25);
-    
-    ellipse(x, y, 66, 66);
+    displayText();
+    keyPressed();
     
 }
 
+function keyPressed() {
+    if (keyCode === RETURN) {
+      wipe();
+      beginGame();
+    }
+}
+
+function wipe() {
+    background(0);
+}
+
+function displayText() {
+    textFont('Chelsea Market')
+    textSize(20);
+    textAlign(CENTER);
+    const beginningText = "Press [ENTER] to begin";
+    fill(255);
+    text(beginningText,windowWidth * .5,windowHeight * .25);
+}
+  
+function drawPluto() {
+    wipe();
+    x = lerp(x,mouseX, 0.05);
+    y = lerp(y,mouseY, 0.05);
+    ellipse(x, y, 50, 50);
+}
+
+function beginGame() {
+    drawPluto();
+}
