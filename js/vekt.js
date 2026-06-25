2eea45fdf76347563bf63bfaaecdc872956a14d2
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2026-06-25 16:18:13 +0300

    vekt trails and neon coloring
    Each vekt gets a random color from a 6-color neon palette (cyan, pink,
    purple, orange, blue, lime). A fading position trail (8 dots) follows
    each vekt. Trail is cleared on screen-wrap to avoid jump lines.

--- js/vekt.js
+++ js/vekt.js
@@ -1,80 +1,96 @@
-// vect.js
-
 class Vekt {
   constructor(x, y, maxSpeed, maxForce) {
     this.position = createVector(x, y);
     this.acceleration = createVector(0, 0);
     this.velocity = createVector(0, 0);
-    // adjust vekt size
     this.size = 4;
     this.maxSpeed = maxSpeed;
     this.maxForce = maxForce;
+    this.trail = [];
+    this.trailLength = 8;
+
+    let neon = [
+      [100, 255, 200],
+      [255, 100, 200],
+      [200, 100, 255],
+      [255, 200, 100],
+      [100, 200, 255],
+      [200, 255, 100]
+    ];
+    this.color = random(neon);
   }
 
   run() {
     this.updateVektLocation();
+    this.updateTrail();
     this.fieldWrap();
     this.display();
   }
 
-  // Craig W. Reynolds' secret sauce
   follow(flowField) {
-    // grab FieldFlow vector at current vekt position
     let desired = flowField.lookup(this.position);
-    // desired *= vekt maxSpeed
     desired.mult(this.maxSpeed);
-    // desired - velocity = steering
     let steer = p5.Vector.sub(desired, this.velocity);
-    // bound turning radius for smooth turns
-    steer.limit(this.maxForce); 
-    // vekt gasoline
+    steer.limit(this.maxForce);
     this.applyForce(steer);
   }
 
-  // vekt go
   applyForce(force) {
     this.acceleration.add(force);
   }
 
   updateVektLocation() {
-    // update/limit velocity at position
     this.velocity.add(this.acceleration);
     this.velocity.limit(this.maxSpeed);
     this.position.add(this.velocity);
-
-    // reset acceleration
     this.acceleration.mult(0);
   }
 
-  // telport vekt to opposite edge if travels off canvas
+  updateTrail() {
+    this.trail.push({ x: this.position.x, y: this.position.y });
+    if (this.trail.length > this.trailLength) {
+      this.trail.shift();
+    }
+  }
+
   fieldWrap() {
+    let wrapped = false;
     if (this.position.x < -this.size) {
       this.position.x = width + this.size;
+      wrapped = true;
     }
     if (this.position.y < -this.size) {
       this.position.y = height + this.size;
+      wrapped = true;
     }
     if (this.position.x > width + this.size) {
       this.position.x = -this.size;
+      wrapped = true;
     }
     if (this.position.y > height + this.size) {
       this.position.y = -this.size;
+      wrapped = true;
+    }
+    if (wrapped) {
+      this.trail = [];
     }
   }
 
   display() {
-    // vekt must rotate in the direction of velocity
-    let theta = this.velocity.heading() + PI / 2;
-    
-    // vekt coloring
-    fill(127);
-    stroke(200);
-    strokeWeight(1);
+    for (let i = 0; i < this.trail.length; i++) {
+      let a = map(i, 0, this.trail.length, 10, 60);
+      let s = map(i, 0, this.trail.length, 1, this.size);
+      fill(this.color[0], this.color[1], this.color[2], a);
+      noStroke();
+      ellipse(this.trail[i].x, this.trail[i].y, s, s);
+    }
 
-    // start new drawing state for vekt
+    let theta = this.velocity.heading() + PI / 2;
     push();
     translate(this.position.x, this.position.y);
     rotate(theta);
+    noStroke();
+    fill(this.color[0], this.color[1], this.color[2]);
     beginShape();
     vertex(0, -this.size * 2);
     vertex(-this.size, this.size * 2);
